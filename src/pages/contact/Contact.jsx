a392ee1ed871e7bc4bd6588b8cc5e61de69c7a30
Lord-Kazekage <generalpurpose112@gmail.com>
2025-10-21 15:15:34 +0500

    i changed only p

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Navbr from "../../componenets/navbar";
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      {" "}
+      <p>Contact</p>
+    </div>
+  );
 }
 
 export default Contact;
